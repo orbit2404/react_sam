5a41366084208979010cb9df298ef892e66bbc93
orbit2404 <jm60115@naver.com>
2024-08-01 12:13:50 +0900

    04_site완성
    04_site완성

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,76 +1,68 @@
 import React, { useState } from 'react'
-
 const headerNav = [
     {
-        title : "intro",
-        url : "#intro",
+        title: "intro",
+        url: "#intro"
     },
     {
-        title : "skip",
-        url : "#skip",
+        title: "skill",
+        url: "#skill"
     },
     {
-        title : "site",
-        url : "#site",
+        title: "site",
+        url: "#site"
     },
     {
-        title : "port",
-        url : "#port",
+        title: "portfolio",
+        url: "#port"
     },
     {
-        title : "contact",
-        url : "#contact",
-    }
+        title: "contact",
+        url: "#contact"
+    },
 ]
+
 
 const Header = () => {
 
-    const [show,setShow] = useState(false)
-    const toggleMenu = () =>{
-        setShow((prevShow) => !prevShow); //! : 반대, 1이면 0으로, 0이면 1로 바꿔주는것 
+    const [show, setShow] = useState(false);
+    const toggleMenu = () => {
+        setShow((prewShow) => !prewShow);
     }
 
-  return (
-    <header id='header' role='banner'>
-        <div className="header_inner">
-            <div className="header_logo">
-                <a href="/">portfolio<em>react</em></a>
+    return (
+        <header id='header' role='banner'>
+            <div className="header_inner">
+                <div className="header_logo">
+                    <a href="/">portfolio <em>react</em></a>
+                </div>
+                <div className={`header_nav ${show ? "show" : ""}`}
+                    role='navigation'
+                    aria-label='메인 메뉴'
+                >
+                    <ul>
+                        {headerNav.map((nav, key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
+
+                    </ul>
+                </div>
+                <div
+                    className="header_nav_moblie"
+                    id='headerToggle'
+                    aria-expanded="false"
+                    aria-controls='primary-menu'
+                    role='button'
+                    tabIndex="0"
+                    onClick={toggleMenu}
+                >
+                    <span></span>
+                </div>
             </div>
-            <div 
-                className={`header_nav ${show ? "show":""}` }                role='navigation'
-                aria-label='메인메뉴'
-            >
-                <ul>
-                    {headerNav.map((nav,key)=>(
-                        <li key={key}>
-                            <a href={nav.url}>{nav.title}</a>
-                        </li> 
-                    ))}                 
-                </ul>
-
-        </div>
-        <div 
-            className="header_nav_mobile"
-            id="headerToggle"
-            aria-controls="primary_menu"
-            aria-expanded={show ? "true":"false"}
-            role="button"
-            tabIndex="0"
-            // tabindex="1"
-            //     문서 안에서 가장 먼저 초점을 받을 수 있습니다. 그러나 자연스러운 마크업 순서를 거스르기 때문에 주의해서 사용해야 합니다. 검색엔진 사이트에서 검색창에 사용하면 적합하지만(이 대신 autofocus 속성이 더 적절할 듯 해요) 그 외 적합한 경우는 잘 떠오르지 않는군요.
-
-            //     tabindex="0"
-            //     키보드 초점을 받을 수 없는 div, span과 같은 요소도 초점을 받을 수 있도록 만들어 줍니다. 초점을 받되 초점을 받는 순서는 자연스러운 마크업 순서를 따릅니다.
-
-            //     tabindex="-1"
-            //     키보드 초점을 받을 수 있는 요소도 초점을 받을 수 없도록 만들어 줍니다. 초점을 받을 수 없기 때문에 "-1" 이외의 다른 음의 정수 값은 사실상 의미가 없습니다.
-            onClick={toggleMenu}
-            >
-                <span></span>
-            </div>
-      </div>
-    </header>
-  )
+        </header>
+    )
 }
 
 export default Header
--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import React from 'react';
+
+
+const siteText = [
+    {
+        text: ["make", "2차 포트폴리오", "HSJ"],
+        title: "AQUARIUM ODYSSEA",
+        code: "/",
+        view: "/",  //여기가 주소
+        info: ["site coding", "production perriod : 5day", "use stack : HTML5 / CSS3 / SCSS / FIGMA"],
+    },
+    {
+        text: ["make", "팀 프로젝트", "CODENAME99"],
+        title: "SPIN PRO SPIN",
+        code: "/",
+        view: "/",  //여기가 주소
+        info: ["site coding", "production perriod : 14day", "use stack : HTML5 / CSS3 / SCSS / FIGMA"],
+    },
+    {
+        text: ["make", "1차 포트폴리오", "HSJ"],
+        title: "GARDEN OF VICTORIAN",
+        code: "/",
+        view: "/",  //여기가 주소
+        info: ["site coding", "production perriod : 14day", "use stack : HTML5 / CSS3 / SCSS / FIGMA"],
+    },
+    {
+        text: ["make", "리디자인", "HSJ"],
+        title: "리디자인 작품이름",
+        code: "/",
+        view: "/",  //여기가 주소
+        info: ["site coding", "production perriod : 3day", "use stack : HTML5 / CSS3 / SCSS / FIGMA"],
+    },
+];
 
 const Site = () => {
-  return (
-    <div>
-      Site
-    </div>
-  )
+    return (
+        <section id='site'>
+            <div className="site_inner">
+                <div className="site_title">
+                    Site coding <em>나의 작품</em>
+                </div>
+                <div className="site_wrap">
+                    {
+                        siteText.map((site, key) => (
+                            <article className={`site_item s${key+1}`} key={key}>
+                                <span className='num'>{key+1} </span>
+                                <div className="text">
+                                    <div>{site.text[0]}</div>
+                                    <div>{site.text[1]}</div>
+                                    <div>{site.text[2]}</div>
+                                </div>
+                                <h3 className='title'>
+                                    {site.title}
+                                </h3>
+                                <div className="btn">
+                                    <a href={site.code}>사이트바로가기</a>
+                                    <a href={site.view}>자세히보기</a>
+                                </div>
+                                <div className="info">
+                                    <span>{site.info[0]}</span>
+                                    <span>{site.info[1]}</span>
+                                    <span>{site.info[2]}</span>
+                                </div>
+                            </article>
+                        ))
+                    }
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Site
